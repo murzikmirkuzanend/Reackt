96736e5550a7331c5380295c78c6275738f16ca6
OlegSaikevych <olegsay@outlook.com>
2022-01-12 01:17:56 +0200

    dome

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
 import './App.css'
 
-import Users from './Componetnts/Users/Users'
-import Posts from './Componetnts/Posts/Posts'
-import Comments from './Componetnts/Comments/Comments'
     ;
 
 function App() {
     return (
 
-        <div className={'heder'}>
-            <div className={'phaser'}>
-            <div className={'user'}>
-                <Users/>
-            </div>
-            
-            <div className={'post'}>
-                <Posts/>
-            </div>
-            </div>
-
-            <div className={'comments'}>
-                <Comments/>
-            </div>
+<div>
 
 
-        </div>
-
+</div>
 
     );
 }
