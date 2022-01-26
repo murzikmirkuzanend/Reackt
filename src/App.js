a7ee0ed36e8805bf1727175edab93e96515d4db0
OlegSaikevych <olegsay@outlook.com>
2022-01-27 01:24:00 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css'
 
-import Users from './Componetnts/Users/Users'
-import Posts from './Componetnts/Posts/Posts'
-import Comments from './Componetnts/Comments/Comments'
-    ;
 
 function App() {
     return (
 
-        <div className={'heder'}>
-            <div className={'phaser'}>
-            <div className={'user'}>
-                <Users/>
-            </div>
-            
-            <div className={'post'}>
-                <Posts/>
-            </div>
-            </div>
-
-            <div className={'comments'}>
-                <Comments/>
-            </div>
+        <div>
 
 
         </div>
