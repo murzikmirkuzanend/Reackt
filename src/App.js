1bb36ce0ad3c2e11ced02e185e7ef443af1b90a6
OlegSaikevych <olegsay@outlook.com>
2022-02-05 02:22:07 +0200

    go

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import './App.css'
 
-import Users from './Componetnts/Users/Users'
-import Posts from './Componetnts/Posts/Posts'
-import Comments from './Componetnts/Comments/Comments'
-    ;
 
 function App() {
     return (
 
-        <div className={'heder'}>
-            <div className={'phaser'}>
-            <div className={'user'}>
-                <Users/>
-            </div>
-            
-            <div className={'post'}>
-                <Posts/>
-            </div>
-            </div>
+        <div >
 
-            <div className={'comments'}>
-                <Comments/>
-            </div>
 
 
         </div>
---
+++ src/home6.ts
@@ -0,0 +1,5 @@
+const func = (str:string):number => {
+  return 5;
+}
+
+func('asd')
